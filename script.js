1200acfd06d910cd08735d7454b6ddc84117d16a
danielocean <kuhtin.vasya@yandex.ru>
2017-07-05 07:27:28 +0700

    1 && 2 fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var temp = new Date()
 // Date
 function time() {
 	temp = new Date()
-	var days = ['Sunday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 	alert( 'Current Date is: '+ days[temp.getDay()]+" "+ temp.getDate() + "." + temp.getMonth() + "." + temp.getFullYear() + " " + temp.getHours() + "h. " + temp.getMinutes() + "m." );
 }
 // converter
@@ -11,10 +11,11 @@ function time() {
 function convertTemp() {
 
 	input = parseInt(document.getElementById("text").value)
-
+	if (!isNaN(input)) {
 	if (document.getElementById("Fahrenheit").checked) temp = parseInt((5/9)*(input-32))
 		else temp = parseInt((9/5)*input + 32)
 	document.getElementById("result").innerHTML = temp
+	} else alert("Incorrect input")
 
 }
 
